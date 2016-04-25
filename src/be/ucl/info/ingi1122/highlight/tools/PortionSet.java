7dab799b0d39b88e42be58806a2ea8c628c4343e
brunoisy <degomme.bruno@gmail.com>
2016-04-25 14:41:05 +0200

    init

---
+++ src/be/ucl/info/ingi1122/highlight/tools/PortionSet.java
@@ -0,0 +1,12 @@
+package be.ucl.info.ingi1122.highlight.tools;
+
+pulic class PortionSet{
+
+	private ArrayList<Portion> portions;
+
+	public PortionSet(){
+		portions = new ArrayList<Portion>();
+	}
+
+	public void addPortion
+}
